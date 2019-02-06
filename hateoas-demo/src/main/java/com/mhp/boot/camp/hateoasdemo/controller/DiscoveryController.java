8a9e262116e37fd6553f82e658a5f7a5a5afec1e
Torsten Hopf <torsten.hopf@mhp.com>
2019-02-06 12:08:30 +0100

    Added Discovery Endpoint

---
+++ hateoas-demo/src/main/java/com/mhp/boot/camp/hateoasdemo/controller/DiscoveryController.java
@@ -0,0 +1,23 @@
+package com.mhp.boot.camp.hateoasdemo.controller;
+
+import org.springframework.hateoas.Link;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+
+@RestController
+@RequestMapping("/")
+public class DiscoveryController {
+    @GetMapping
+    public List<Link> discover() {
+        Link wife = linkTo(WifeController.class).withRel("wifes");
+        return Arrays.asList(wife);
+    }
+
+
+}
