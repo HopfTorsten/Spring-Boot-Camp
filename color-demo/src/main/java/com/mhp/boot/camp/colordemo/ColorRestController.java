9310f99ca4d44d6d2a37445d1c72e8404ecac4f2
Torsten Hopf <torsten.hopf@mhp.com>
2019-02-07 11:48:51 +0100

    Added @RefreshScope for reloading the ocntext

--- color-demo/src/main/java/com/mhp/boot/camp/colordemo/ColorRestController.java
+++ color-demo/src/main/java/com/mhp/boot/camp/colordemo/ColorRestController.java
@@ -1,16 +1,25 @@
 package com.mhp.boot.camp.colordemo;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/color")
+@RefreshScope
 public class ColorRestController {
 
-    @GetMapping
+    @Value("${msgText:Hello World!}")
+    private String msgText;
+
+    @GetMapping("/color")
     public String getColor() {
         return "BLUE";
+    }
+
+    @GetMapping("/message")
+    public String getMessage() {
+        return msgText;
     }
 
 }
