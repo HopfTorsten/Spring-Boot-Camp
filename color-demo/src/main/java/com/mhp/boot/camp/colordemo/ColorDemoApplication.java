6d6f7824b17ba4b6ed4bf3b96bc0e84db6ba09be
Torsten Hopf <torsten.hopf@mhp.com>
2019-02-07 13:30:03 +0100

    Enabled Eureka Client

--- color-demo/src/main/java/com/mhp/boot/camp/colordemo/ColorDemoApplication.java
+++ color-demo/src/main/java/com/mhp/boot/camp/colordemo/ColorDemoApplication.java
@@ -2,7 +2,9 @@ package com.mhp.boot.camp.colordemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ColorDemoApplication {
 
