3204f5b91e71ba33d0cbbe1c2bd7caa43704dfed
Torsten Hopf <torsten.hopf@mhp.com>
2019-02-07 12:17:28 +0100

    Added Eureka Server basic

---
+++ eureka-demo/src/main/java/com/boot/camp/eurekademo/EurekaDemoApplication.java
@@ -0,0 +1,16 @@
+package com.boot.camp.eurekademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaDemoApplication.class, args);
+    }
+
+}
+
---
+++ eureka-demo/src/test/java/com/boot/camp/eurekademo/EurekaDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.boot.camp.eurekademo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
