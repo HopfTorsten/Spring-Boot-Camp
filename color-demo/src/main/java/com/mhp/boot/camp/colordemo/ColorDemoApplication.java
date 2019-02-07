b4ee46cf51b5ff49888579a7a70ad7947d626d78
Torsten Hopf <torsten.hopf@mhp.com>
2019-02-07 10:56:23 +0100

    Initial

---
+++ color-demo/src/main/java/com/mhp/boot/camp/colordemo/ColorDemoApplication.java
@@ -0,0 +1,14 @@
+package com.mhp.boot.camp.colordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColorDemoApplication.class, args);
+    }
+
+}
+
---
+++ color-demo/src/test/java/com/mhp/boot/camp/colordemo/ColorDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.mhp.boot.camp.colordemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ColorDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
